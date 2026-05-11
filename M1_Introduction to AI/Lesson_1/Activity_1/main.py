cb7e6494a48b9077b7aba1304056cbbeb9e9e0de
harinegopalakrishnan <harinegopalakrishnan@gmail.com>
2026-05-11 21:47:09 +0530

    Test change

--- M1_Introduction to AI/Lesson_1/Activity_1/main.py
+++ M1_Introduction to AI/Lesson_1/Activity_1/main.py
@@ -1,7 +1,7 @@
 #Title: Hello AI
 
 name = input("Hello! I am AI Bot, Please enter you name: ")
-print(f"Nice to meet you,{name} !!!")
+print(f"Nice to meet you, {name} !!!")
 print("How are you feeling today (Good/Bad)?: ")
 mood = input().lower()
 
